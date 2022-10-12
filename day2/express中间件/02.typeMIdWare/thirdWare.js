701b013ee78a0d23173018d89f9277813c2ea103
hanxuehong <hanxuehong@excean.com>
2022-10-12 08:39:21 +0800

    中间件类型的补充

---
+++ day2/express中间件/02.typeMIdWare/thirdWare.js
@@ -0,0 +1,39 @@
+/**
+ * 第三方中间件
+ * 需要下载才能使用，非官方内置
+ * eg: body-parse 用来解析请求体数据
+ */
+ let express = require('express')
+ let bodyParser = require('body-parser')
+ 
+ let app = express()
+ 
+ //  express@4.16.0版本开始 可以通过express.urlencoded这个中间件解析表单中的url-encoded格式的数据
+ // 内置中间件的使用
+ // app.use(express.urlencoded({extended: false}))
+ app.use(bodyParser.urlencoded({extended: false}))
+ // 在express@4.16.0 之前的版本中也可以通过第三方
+ 
+ app.post('/process_post',(req,res) => {
+     console.log('测试请求相关的数据',req.body)
+    // 输出 JSON 格式
+    var response = {
+         "first_name":req.body.first_name,
+         "last_name":req.body.last_name
+     };
+     // console.log(response);
+ 
+     // 设置响应头  避免中文乱码
+     res.setHeader('Content-Type', 'text/html; charset=utf-8')
+     res.send(response);
+ })
+ app.get('/index.html', function (req, res) {
+     res.sendFile( __dirname + "/" + "index.html" );
+  })
+ 
+ let server = app.listen(3000,'localhost',() => {
+     let host = server.address().address
+     let port = server.address().port
+ 
+     console.log(`当前实例运行的地址为: http://${host}:${port}`)
+ })
